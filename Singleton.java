ce1cc53338ade7abd6ee251208064acb5bf32f7d
Ishwarya1219 <ishwaryasri1912@gmail.com>
2025-06-16 08:10:30 +0530

    SingletonExercise

--- Singleton.java
+++ Singleton.java
@@ -1,5 +1,26 @@
 public class Singleton {
+    private static Singleton instance;
+    private Singleton(){
+          System.out.println("Logger initialized");
+    }
+    public static Singleton getInstance(){
+        if(instance==null){
+            instance=new Singleton();
+        }
+        return instance;
+    }
+   
     public static void main(String args[]){
-        System.out.print("hello");
+        Singleton log1=Singleton.getInstance();
+    
+        Singleton log2=Singleton.getInstance();
+      
+        if(log1==log2){
+            System.out.print("Both has same instance");
+
+        }
+        else{
+            System.out.println("Different instances");
+        }
     }
 }
